aea63e98554fc29f0eb8942211678b0d600ae89f
kw78999 <kw78999@naver.com>
2021-01-15 16:37:34 +0900

    2021_01_15

--- JSP/myapp/src/member/DBConnectionMgr.java
+++ JSP/myapp/src/member/DBConnectionMgr.java
@@ -39,7 +39,7 @@ import java.util.Vector;
 public class DBConnectionMgr {
     private Vector connections = new Vector(10);
     private String _driver = "org.gjt.mm.mysql.Driver",
-    _url = "jdbc:mysql://127.0.0.1:3306/mydb?useUnicode=true&characterEncoding=EUC_KR",
+    _url = "jdbc:mysql://127.0.0.1:3306/naver?useUnicode=true&characterEncoding=EUC_KR",
     _user = "root",
     _password = "1234";
     
--- JSP/myapp/src/member/MemberMgr.java
+++ JSP/myapp/src/member/MemberMgr.java
@@ -1,20 +1,123 @@
 package member;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Vector;
+
+import member.MemberBean;
 
 public class MemberMgr {
 	
 	private DBConnectionMgr pool;
 	
-	public MemberMgr() {
+	public MemberMgr(){
 		pool = DBConnectionMgr.getInstance();
 	}
-	
-	//로그인 : 성공 -> true
-	//select id from tblMember where id=? and pwd=?
+	//아이디 찾기
+	public String findId(String name, String phone) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String str = null;
+		try {
+			con = pool.getConnection();
+			sql = "select id from navermember where name=? and mobile=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, phone);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				str = rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return str;
+	}
+	//비밀번호 찾기
+	public String findPwd(String id, String name, String phone) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String str = null;
+		try {
+			con = pool.getConnection();
+			sql = "select pwd from navermember where id=? and name=? and mobile=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, name);
+			pstmt.setString(3, phone);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				str = rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return str;
+	}
+	//비밀번호 수정
+	public boolean updatePassword(String id, String pwd, String newPwd) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "select * from navermember where id=? and pwd=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			rs = pstmt.executeQuery();			
+			if(rs.next())
+				pstmt.close();
+				sql = "UPDATE navermember SET pwd=? WHERE id=? AND pwd=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, newPwd);
+				pstmt.setString(2, id);
+				pstmt.setString(3, pwd);				
+				int cnt = pstmt.executeUpdate();
+				if(cnt==1)
+					flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	}
+	//회원탈퇴
+	public boolean deleteMember(String id, String pwd) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "DELETE from navermember WHERE id=? and pwd=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			int cnt = pstmt.executeUpdate();
+			if(cnt==1)
+				flag=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	}
+	////로그인
 	public boolean loginMember(String id, String pwd) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -23,10 +126,32 @@ public class MemberMgr {
 		boolean flag = false;
 		try {
 			con = pool.getConnection();
-			sql = "select id from tblMember where id=? and pwd=?";
+			sql = "select id from NaverMember where id=? and pwd=?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pwd);
+			rs = pstmt.executeQuery();
+			//if(rs.next()) flag=true;
+			flag = rs.next();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return flag;
+	}
+	//id 중복체크
+	public boolean checkId(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "select id from NaverMember where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			flag = rs.next();
 		} catch (Exception e) {
@@ -36,60 +161,31 @@ public class MemberMgr {
 		}
 		return flag;
 	}
-	
-	//id 중복 체크 : 중복 -> true
-	//select id from tblMember where id=?
-	public boolean checkId(String id) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-		boolean flag= false;
-		try {
-			con = pool.getConnection();
-			sql = "select id from tblMember where id=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			flag = rs.next();//결과값이 있다면 true <- 중복
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt, rs);
-		}
-		return flag;
-	}
-	
-	//주소검색
-	//select * from tblZipcode where area3 like ?
-	public Vector<ZipcodeBean> searchZipcode(String area3){
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-		Vector<ZipcodeBean> vlist = new Vector<ZipcodeBean>();
-		try {
-			con = pool.getConnection();
-			sql = "select * from tblZipcode where area3 like ?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, "%"+area3+"%");// '%강남대로%'
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				ZipcodeBean bean = new ZipcodeBean();
-				bean.setZipcode(rs.getString(1));
-				bean.setArea1(rs.getString(2));
-				bean.setArea2(rs.getString(3));
-				bean.setArea3(rs.getString(4));
-				vlist.addElement(bean);//vlist.add 가능
+	//모든 id 가져오기
+		public Vector<MemberBean> getid() {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			Vector<MemberBean> vlist = new Vector<MemberBean>();
+			try {
+				con = pool.getConnection();
+				sql = "select id from NaverMember";
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+			
+				while(rs.next()) {
+					MemberBean bean = new MemberBean();
+					bean.setId(rs.getString("id"));
+					vlist.addElement(bean);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt, rs);
+			return vlist;
 		}
-		return vlist;
-	}
-	
 	//회원가입
 	public boolean insertMember(MemberBean bean) {
 		Connection con = null;
@@ -98,132 +194,23 @@ public class MemberMgr {
 		boolean flag = false;
 		try {
 			con = pool.getConnection();
-			sql = "insert tblMember(id,pwd,name,gender,"
-					+ "birthday,email,zipcode,address,hobby,job)"
-					+ "values(?,?,?,?,?,?,?,?,?,?)";
+			sql = "insert NaverMember(id, pwd, name, birth_yyyy, birth_mm, birth_dd, gender, phone) values(?,?,?,?,?,?,?,?)";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, bean.getId());
-			pstmt.setString(2, bean.getPwd());
-			pstmt.setString(3, bean.getName());
-			pstmt.setString(4, bean.getGender());
-			pstmt.setString(5, bean.getBirthday());
-			pstmt.setString(6, bean.getEmail());
-			pstmt.setString(7, bean.getZipcode());
-			pstmt.setString(8, bean.getAddress());
-			/////////////////////////////////
-			String lists[] = {"인터넷", "여행", "게임", "영화", "운동"};
-			String hobby[] = bean.getHobby();//인터넷 게임 운동 -> 10101
-			char hb[] = {'0','0','0','0','0'};
-			for (int i = 0; i < hobby.length; i++) {//3
-				for (int j = 0; j < lists.length; j++) {//5
-					if(hobby[i].equals(lists[j])) {
-						hb[j] = '1';
-						break;
-					}
-				}
-			}
-			pstmt.setString(9, new String(hb));//"10101"
-			/////////////////////////////////
-			pstmt.setString(10, bean.getJob());
-			if(pstmt.executeUpdate()==1) flag = true;
+			pstmt.setString(1,bean.getId());
+			pstmt.setString(2,bean.getPwd());
+			pstmt.setString(3,bean.getName());;
+			pstmt.setString(4,bean.getBirth_yyyy());
+			pstmt.setString(5,bean.getBirth_mm());
+			pstmt.setString(6,bean.getBirth_dd());
+			pstmt.setString(7,bean.getGender());
+			pstmt.setString(8,bean.getPhone());
+			if(pstmt.executeUpdate() == 1) flag = true;
 		} catch (Exception e) {
-			e.printStackTrace();
+				e.printStackTrace();
 		} finally {
-			pool.freeConnection(con, pstmt);
+				pool.freeConnection(con, pstmt);
 		}
 		return flag;
 	}
 	
-	//회원정보가져오기
-	//취미를 DB에는 문자열 -> 빈즈는 배열이므로 신경.
-	public MemberBean getMember(String id) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-		MemberBean bean = new MemberBean();
-		try {
-			con = pool.getConnection();
-			sql = "select * from tblMember where id = ?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				bean.setId(rs.getString("id"));
-				bean.setPwd(rs.getString("pwd"));
-				bean.setName(rs.getString("name"));
-				bean.setGender(rs.getString("gender"));
-				bean.setBirthday(rs.getString("birthday"));
-				bean.setEmail(rs.getString("email"));
-				bean.setZipcode(rs.getString("zipcode"));
-				bean.setAddress(rs.getString("address"));
-				///////////////////////////////////////
-				String hobby = rs.getString("hobby");//10101
-				String hb[] = new String[hobby.length()];
-				for (int i = 0; i < hb.length; i++) {
-					hb[i] = hobby.substring(i, i+1);
-				}
-				bean.setHobby(hb);
-				///////////////////////////////////////
-				bean.setJob(rs.getString("job"));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt, rs);
-		}
-		return bean;
-	}
-	
-	//회원정보수정
-	public boolean updateMember(MemberBean bean) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		String sql = null;
-		boolean flag = false;
-		try {
-			con = pool.getConnection();
-			sql = "update tblMember set pwd=?,name=?,gender=?,"
-					+ "birthday=?,email=?,zipcode=?,address=?,hobby=?"
-					+ ",job=? where id=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, bean.getPwd());
-			pstmt.setString(2, bean.getName());
-			pstmt.setString(3, bean.getGender());
-			pstmt.setString(4, bean.getBirthday());
-			pstmt.setString(5, bean.getEmail());
-			pstmt.setString(6, bean.getZipcode());
-			pstmt.setString(7, bean.getAddress());
-			/////////////////////////////////
-			String lists[] = {"인터넷", "여행", "게임", "영화", "운동"};
-			String hobby[] = bean.getHobby();//인터넷 게임 운동 -> 10101
-			char hb[] = {'0','0','0','0','0'};
-			for (int i = 0; i < hobby.length; i++) {//3
-				for (int j = 0; j < lists.length; j++) {//5
-					if(hobby[i].equals(lists[j])) {
-						hb[j] = '1';
-						break;
-					}
-				}
-			}
-			pstmt.setString(8, new String(hb));//"10101"
-			/////////////////////////////////
-			pstmt.setString(9, bean.getJob());
-			pstmt.setString(10, bean.getId());
-			if(pstmt.executeUpdate()==1) flag = true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt);
-		}
-		return flag;
-	}
 }
-
-
-
-
-
-
-
-
